d963a35ead28b901ad3685d10a4262677f03f8d5
DaanV2 <daanverstraten@hotmail.com>
2022-09-06 20:00:23 +0200

    Added

--- source/Package.ts
+++ source/Package.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import { CreateMcWorld } from "./Actions/Archive.ts/CreateMcWorld";
 import { TrimFiles } from "./Actions/Files/Trimming";
 import { JsonProcessing } from "./Actions/Json/Processing";
@@ -6,8 +5,8 @@ import { PackageContext } from "./types/PackageContext";
 
 /**
  * Returns the path to the mcworld
- * @param Context
- * @returns
+ * @param Context The context of the package
+ * @returns The path to the mcworld
  */
 export function Package(Context: PackageContext): string {
   if (Context.ProcessJson) {
