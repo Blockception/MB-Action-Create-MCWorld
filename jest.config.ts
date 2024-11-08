cfdfd4c38bef4675d86686b416c929298bd8faed
DaanV2 <daanverstraten@hotmail.com>
2024-11-08 11:32:08 +0100

    adding jest

--- jest.config.ts
+++ jest.config.ts
@@ -128,7 +128,8 @@ const config: Config = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "./src",
+    "./source",
+    "./tests"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
