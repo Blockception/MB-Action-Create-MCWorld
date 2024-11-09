43824acfa77fbc5d8b7a1c867996d5404031fe45
DaanV2 <daanverstraten@hotmail.com>
2024-11-09 23:10:14 +0100

    updating config

--- jest.config.ts
+++ jest.config.ts
@@ -28,10 +28,7 @@ const config: Config = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "\\\\node_modules\\\\",
-    "\\\\coverage\\\\"
-  ],
+  coveragePathIgnorePatterns: ["node_modules", "coverage"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
